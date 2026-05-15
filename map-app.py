ee323a6771b1f621f7522fba121e153f9f2d75d2
rodricar25 <acarneiro.rodrigo@gmail.com>
2026-05-15 19:35:25 -0300

    Uncomment app.run for production deployment

--- map-app.py
+++ map-app.py
@@ -170,5 +170,6 @@ def index():
     """, graph=graph_html)
 
 
-#if __name__ == "__main__":
+if __name__ == "__main__":
+    app.run(debug=False)
 #    app.run(host="0.0.0.0", port=5000, debug=True)
