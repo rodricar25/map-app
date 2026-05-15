700666ee7dc67005528e145291cab164a4ffe93b
rodricar25 <acarneiro.rodrigo@gmail.com>
2026-05-15 19:31:33 -0300

    Update map-app.py

--- map-app.py
+++ map-app.py
@@ -170,5 +170,5 @@ def index():
     """, graph=graph_html)
 
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=True)
+#if __name__ == "__main__":
+#    app.run(host="0.0.0.0", port=5000, debug=True)
